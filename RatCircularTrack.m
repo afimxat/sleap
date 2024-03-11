b891425a3d06fdbb575ab32edb245c8f008a2b04
Utku Kaya <kayautku@gmail.com>
2024-03-11 17:34:13 -0400

    updates

--- RatCircularTrack.m
+++ RatCircularTrack.m
@@ -2,6 +2,7 @@ classdef RatCircularTrack < SleapHDF5Loader
     properties
         % Add properties specific to RatCircularTrack analysis here
         PositionTable
+        Center
         Radius
     end
 
@@ -32,9 +33,179 @@ classdef RatCircularTrack < SleapHDF5Loader
         end
 
         % Example method to calculate center of circular track
-        function obj = getHeadPosition(obj)
-            % Dummy implementation - replace with actual calculation
+        function obj = plotRawTime(obj)
+            pbaspect([20 1 1])
+            hold on
+            pt=obj.PositionTable;
+            color1=linspecer(20,'sequential');
+            size1=[2 5 10 1]*3;
+            alpha=[.5 .2 .1 .1];
+            fr=25;
+            nodes=unique(pt.Node);
+            for inode=1:numel(nodes)
+                pt1=pt(pt.Node==inode,:);
+                x1=pt1.XCoordinate;
+                y1=pt1.YCoordinate;
+                time1=pt1.Frame/fr/60;
+                scatter3(time1,x1,y1,size1(inode),color1(inode,:), ...
+                    "filled", MarkerEdgeAlpha=alpha(inode),MarkerFaceAlpha=alpha(inode))
+            end
+            scatter3(time1, ...
+                ones(size(time1))*obj.Center(1), ...
+                ones(size(time1))*obj.Center(2), ...
+                30,[.5 .5 .5],"filled", ...
+                MarkerEdgeAlpha=.5,MarkerFaceAlpha=.5);
+            ax=gca;
+            ax.YLim=[0 1000];
+            ax.ZLim=[0 1000];
+        end        % Example method to calculate center of circular track
+        function obj = plotAngleTime(obj)
+            ax=gca;
+            pbaspect([20 1 1])
+            hold on
+            pt=obj.PositionTable;
+            color1=linspecer(20,'sequential');
+            size1=[5 5 1 1]*3;
+            alpha=[.1 .5 .1 .1];
+            fr=25;
+            nodes=unique(pt.Node);
+            for inode=1:numel(nodes)
+                pt1=pt(pt.Node==inode,:);
+                x1=pt1.XCoordinate;
+                y1=pt1.YCoordinate;
+                % Adjust coordinates to be relative to the center
+                x_relative = x1 - obj.Center(1);
+                y_relative = y1 - obj.Center(2);
 
+                % Calculate the angle in radians
+                angleRadians = -atan2(y_relative, x_relative);
+                % Convert the angle to degrees
+                angleDegrees = rad2deg(angleRadians);
+                % Adjust angles to be within the range [0, 360) if necessary
+                % angleDegrees = mod(angleDegrees, 360);
+                time1=pt1.Frame/fr/60;
+                scatter(time1,angleDegrees,size1(inode),color1(inode,:), ...
+                    "filled", MarkerEdgeAlpha=alpha(inode),MarkerFaceAlpha=alpha(inode))
+            end
+        end
+        function obj = plotHeadDirectionColor(obj)
+            ax=gca;
+            pbaspect([20 1 1])
+            hold on
+            pt=obj.PositionTable;
+            fr=25;
+            pt1=pt(pt.Node==1,:);
+            x1=pt1.XCoordinate;
+            y1=pt1.YCoordinate;
+            pt2=pt(pt.Node==2,:);
+            x2=pt2.XCoordinate;
+            y2=pt2.YCoordinate;
+            % Step 1: Calculate the midpoint M
+            mx = (x1 + x2) / 2;
+            my = (y1 + y2) / 2;
+            % Adjust coordinates to be relative to the center
+            x_relative = mx - obj.Center(1);
+            y_relative = my - obj.Center(2);
+
+            % Calculate the angle in radians
+            angleRadians = -atan2(y_relative, x_relative);
+            % Convert the angle to degrees
+            angleDegrees = rad2deg(angleRadians);
+
+            % Step 2: Calculate the direction (angle) of the vector from P1 to P2
+            vectorAngleDegrees = rad2deg(atan2(y2 - y1, x2 - x1));
+
+            % Calculate vector from center to midpoint
+            cx = mx - obj.Center(1);
+            cy = my - obj.Center(2);
+
+            % Calculate the direction (angle) of this vector
+            centerToMidpointAngleDegrees = rad2deg(atan2(cy, cx));
+
+            % Step 3: Calculate the angle between the two vectors
+            angleBetweenVectors = mod(centerToMidpointAngleDegrees - vectorAngleDegrees, 360);
+
+            % % Ensure the angle is within the range [0, 180]
+            % if angleBetweenVectors > 180
+            %     angleBetweenVectors = 360 - angleBetweenVectors;
+            % end
+            % Normalize angle to [0, 1]
+            normalizedAngle = angleBetweenVectors / 360;
+
+            % Generate HSV colormap
+            numColors = 256; % You can adjust the number of colors
+            hsvColors = hsv(numColors);
+
+            % Map normalized angle to the colormap
+            % Ensure the indices are within the valid range of the colormap
+            colorIndices = ceil(normalizedAngle * (numColors - 1)) + 1;
+            colorIndices(isnan(colorIndices))=1;
+            colorVector = hsvColors(colorIndices, :);
+
+            time1=pt1.Frame/fr/60;
+            scatter(time1,angleDegrees,ones(size(time1))*5,colorVector, ...
+                "filled", MarkerEdgeAlpha=.2,MarkerFaceAlpha=.2)
+        end
+        function obj = plotHeadDirection(obj)
+            ax=gca;
+            pbaspect([20 1 1])
+            hold on
+            pt=obj.PositionTable;
+            fr=25;
+            pt1=pt(pt.Node==1,:);
+            x1=pt1.XCoordinate;
+            y1=pt1.YCoordinate;
+            pt2=pt(pt.Node==2,:);
+            x2=pt2.XCoordinate;
+            y2=pt2.YCoordinate;
+            % Step 1: Calculate the midpoint M
+            mx = (x1 + x2) / 2;
+            my = (y1 + y2) / 2;
+            % Adjust coordinates to be relative to the center
+            x_relative = mx - obj.Center(1);
+            y_relative = my - obj.Center(2);
+
+            % Calculate the angle in radians
+            angleRadians = -atan2(y_relative, x_relative);
+            % Convert the angle to degrees
+            angleDegrees = rad2deg(angleRadians);
+
+            % Step 2: Calculate the direction (angle) of the vector from P1 to P2
+            vectorAngleDegrees = rad2deg(atan2(y2 - y1, x2 - x1));
+
+            % Calculate vector from center to midpoint
+            cx = mx - obj.Center(1);
+            cy = my - obj.Center(2);
+
+            % Calculate the direction (angle) of this vector
+            centerToMidpointAngleDegrees = rad2deg(atan2(cy, cx));
+
+            % Step 3: Calculate the angle between the two vectors
+            angleBetweenVectors = mod(centerToMidpointAngleDegrees - vectorAngleDegrees, 360);
+
+            % Normalize angle to [0, 1]
+            normalizedAngle = angleBetweenVectors / 360;
+
+            % Generate HSV colormap
+            numColors = 256; % You can adjust the number of colors
+            hsvColors = hsv(numColors);
+
+            % Map normalized angle to the colormap
+            % Ensure the indices are within the valid range of the colormap
+            colorIndices = ceil(normalizedAngle * (numColors - 1)) + 1;
+            colorIndices(isnan(colorIndices))=1;
+            colorVector = hsvColors(colorIndices, :);
+
+            time1=pt1.Frame/fr/60;
+
+            scatter(time1,abs(mod(angleBetweenVectors+180,360)-180),ones(size(time1))*5,colorVector, ...
+                "filled", MarkerEdgeAlpha=.2,MarkerFaceAlpha=.2)
+        end
+        % Example method to calculate center of circular track
+        function obj = setCenter(obj,center)
+            obj.Center=center;
+        end
+        function obj = getHeadPosition(obj)
         end
         % Example method to calculate center of circular track
         function obj = calculateTrackCenter(obj)
--- testingSleapAnalysis.m
+++ testingSleapAnalysis.m
@@ -1,30 +1,55 @@
-folder='D:\sleap\TrackModels\export';
-filepath=fullfile(folder,"labels.v002.004_Basler_acA4024-29um__24844056__20240124_142448039_1.analysis.h5");
-loader = RatCircularTrack(filepath);
-head=loader.getHeadPosition;
-% Number of frames
-numFrames = size(loader.Tracks, 1);
 
-% Create a figure for the animation
-figure;
-    clf; % Clear current figure window
-ax=gca;
-ax.YDir="reverse";
-    axis equal;
-for frameIndex = 5000:numFrames
-    positions = squeeze(loader.Tracks(frameIndex, :, :, 1));
-    
-    hold on;
-    % plot(positions(:, 1), positions(:, 2), 'o'); % Plot points for current frame
-    
-    % Draw edges
-    for i = 1:size(positions, 1)-3
-        plot(positions(i:i+1, 1), positions(i:i+1, 2), 'k-');
-    end
-    
+
+folder='D:\sleap\TrackModels\export';
+files=dir(fullfile(folder,'*.h5'));
+for ifile=1:numel(files)
+    filename1=files(ifile).name;
+    filepath=fullfile(folder,filename1);
+    ratontrack = RatCircularTrack(filepath);
+    ratontrack=ratontrack.setCenter([550 500]);
+    ff=logistics.FigureFactory.instance(folder);ff.ext={'.png'};ff.resolution=600;
+    figure(1);clf
+    % ratontrack1=ratontrack.getHeadPosition
+    ax=gca;
+    ax.ZDir="reverse";
+    ax.XDir="reverse";
+    ratontrack.plotRawTime;
+    view(90,-60)
+    ff.save(strcat(filename1,'_raw.png'))
+    figure(2);clf; tiledlayout("vertical","TileSpacing","none");t1=nexttile;
+    ratontrack.plotRawTime;
+    view(-30,0)
+    t2=nexttile;
+    ratontrack.plotAngleTime;
+    t3=nexttile;
+    ratontrack.plotHeadDirectionColor;
+    t4=nexttile;
+    ratontrack.plotHeadDirection;
+    linkaxes([t1 t2 t3 t4],'x')
+    ff.save(strcat(filename1,'_angle+direction.png'))
 end
-    xlabel('X Position');
-    ylabel('Y Position');
-    title(sprintf('Edges for Frame %d', frameIndex));
-    % pause(0.1); % Pause to create an animation effect
-    hold off;
+% head=ratontrack.getHeadPosition;
+% 
+% % Number of frames
+% numFrames = size(ratontrack.Tracks, 1);
+% 
+% % Create a figure for the animation
+%     clf; % Clear current figure window
+% 
+% for frameIndex = 5000:numFrames
+%     positions = squeeze(ratontrack.Tracks(frameIndex, :, :, 1));
+% 
+%     hold on;
+%     % plot(positions(:, 1), positions(:, 2), 'o'); % Plot points for current frame
+% 
+%     % Draw edges
+%     for i = 1:size(positions, 1)-3
+%         plot(positions(i:i+1, 1), positions(i:i+1, 2), 'k-');
+%     end
+% 
+% end
+%     xlabel('X Position');
+%     ylabel('Y Position');
+%     title(sprintf('Edges for Frame %d', frameIndex));
+%     % pause(0.1); % Pause to create an animation effect
+%     hold off;
